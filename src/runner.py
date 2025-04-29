8575380cce75448c97f6a70b42536e9593e951bb
Martin Shin <martinshin95@gmail.com>
2025-04-29 16:59:52 +0800

    simple tweak to humanoid apple pick env

--- src/runner.py
+++ src/runner.py
@@ -2,21 +2,35 @@ import os
 import gymnasium as gym
 import time
 from mani_skill.utils.wrappers.record import RecordEpisode
-# TODO: Change this to import your env
-from examples.card_stack_env import CardStackEnv
-from zucan_lv.simple_office_env import SimpleOfficeEnv
 
-def generate_videos(n_episodes=10, max_steps_per_episode=100, video_dir="card_stack_videos"):
+# TODO: Change this to import your env
+from marshin.kitchen_env import HeatupFoodEnv
+from marshin.humanoid_pick_place import UnitreeG1PlaceAppleInBowlEnhancedEnv
+
+# from zucan_lv.simple_office_env import SimpleOfficeEnv
+
+
+def generate_videos(
+    n_episodes=10, max_steps_per_episode=100, video_dir="card_stack_videos"
+):
     """
     Generate and save videos of random agent interactions in the CardStack environment.
     """
     # TODO: Change this to make your en
-    env = gym.make("SimpleOffice-v1", obs_mode="state", render_mode="rgb_array")
+    # SimpleOffice-v1
+    env = gym.make(
+        "UnitreeG1PlaceAppleInBowlEnhanced-v1", obs_mode="state", render_mode="rgb_array"
+    )
     video_dir = os.path.join(video_dir, time.strftime("%Y%m%d-%H%M%S"))
     os.makedirs(video_dir, exist_ok=True)
 
-    env = RecordEpisode(env, output_dir=video_dir, save_video=True, 
-                        trajectory_name="random_actions", max_steps_per_video=max_steps_per_episode)
+    env = RecordEpisode(
+        env,
+        output_dir=video_dir,
+        save_video=True,
+        trajectory_name="random_actions",
+        max_steps_per_video=max_steps_per_episode,
+    )
     for _ in range(n_episodes):
         obs, info = env.reset()
         for _ in range(max_steps_per_episode):
@@ -26,5 +40,8 @@ def generate_videos(n_episodes=10, max_steps_per_episode=100, video_dir="card_st
                 break
     env.close()
 
+
 if __name__ == "__main__":
-    generate_videos(n_episodes=1, max_steps_per_episode=1000, video_dir="maniskill_videos")
+    generate_videos(
+        n_episodes=1, max_steps_per_episode=1000, video_dir="maniskill_videos"
+    )
